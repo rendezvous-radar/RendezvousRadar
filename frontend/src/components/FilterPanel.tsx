4ce995c0b90165113cd3b2d8065b9663281e51bf
Jinha <56976518+JinhaKimGH@users.noreply.github.com>
2024-08-21 00:03:48 -0400

    Updated frontend with search bar and filter panel hide/show capabilities

---
+++ frontend/src/components/FilterPanel.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * SearchBar Component
+ *  
+ * @param {Object} props - The component props.
+ * @returns {JSX.Element} A React JSX element representing the FilterPanel Component, the filter panel of the website
+*/
+export default function FilterPanel(props: {}) : JSX.Element {
+    return ( 
+        <div>
+            Filter Panel
+        </div>
+    );
+}
---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../logo.png';
+
+/**
+ * SearchBar Component
+ *  
+ * @param {Object} props - The component props.
+ * @returns {JSX.Element} A React JSX element representing the SearchBar Component, the search bar of the website
+*/
+export default function SearchBar(props: {}) : JSX.Element {
+    return ( 
+        <div className="SearchBar">
+            <span className="material-icons searchIcon">search</span>
+            <input className='searchInput'></input>
+        </div>
+    );
+}
