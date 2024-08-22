b81b1d9a7448c2ab5af9115bda2d175febae8015
Jinha <56976518+JinhaKimGH@users.noreply.github.com>
2024-08-21 23:23:41 -0400

    Added apply filter button, requires error handling and API call to be integrated

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,10 +1,9 @@
 /**
  * SearchBar Component
  *  
- * @param {Object} props - The component props.
  * @returns {JSX.Element} A React JSX element representing the SearchBar Component, the search bar of the website
 */
-export default function SearchBar(props: {}) : JSX.Element {
+export default function SearchBar() : JSX.Element {
     return ( 
         <div className="SearchBar">
             <span className="material-icons searchIcon">search</span>
